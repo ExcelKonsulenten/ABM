8bfd48da900b3aeb01e38c72f3e27cf630812b48
ExcelKonsulenten <prosjekt@excelkonsulenten.no>
2025-10-06 13:13:28 +0200

    Update Start app (dobbelklikk).bat

--- Start app (dobbelklikk).bat
+++ Start app (dobbelklikk).bat
@@ -1,10 +1,1 @@
-@echo off
-setlocal
-pushd "%~dp0"
-if not exist ".venv" (
-  py -3 -m venv .venv
-)
-call ".venv\Scripts\activate"
-python -m pip install --upgrade pip
-python start_app.py
-pause
+
